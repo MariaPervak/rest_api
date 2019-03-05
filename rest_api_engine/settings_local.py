0700aa582b4fefc053f9370778498941afcf0878
Pervak Maria <pervak.maria@gmail.com>
2019-03-06 00:16:26 +0400

    configuration

---
+++ rest_api_engine/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rest_api_db',
+        'USER' : 'admin',
+        'PASSWORD' : '2128506',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
+    }
+}
